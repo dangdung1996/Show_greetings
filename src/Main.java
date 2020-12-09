fd1f14e2e3e415d4ed400e4112e48455f3e8710d
dangdung1996 <dung1996.it@gmail.com>
2020-12-09 17:08:03 +0700

    add file main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
